816d78eef877c97e7fd725e9f0a0d0bde06b4a98
Eddie Naff <eddienaff@gmail.com>
2023-03-24 13:32:02 -0700

    Add utility promise wrapper

---
+++ src/utils.ts/async-wrapper.ts
@@ -0,0 +1,11 @@
+export async function asyncWrapper<T>(
+  fn: (...args: any[]) => Promise<T>,
+  ...args: any
+): Promise<[unknown | null, T | null]> {
+  try {
+    const data = await fn(...args);
+    return [null, data];
+  } catch (error) {
+    return [error, null];
+  }
+}
