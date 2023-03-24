c3f98c3b23ccd9aad81064fc4e70d28fc905bf06
Eddie Naff <eddienaff@gmail.com>
2023-03-24 15:28:56 -0700

    Add sentiment analysis to app

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { z as zod } from "zod";
+import { createZodFetcher } from "zod-fetch";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
+import positive from "./assets/img/positive.svg";
+import neutral from "./assets/img/neutral.svg";
+import negative from "./assets/img/negative.svg";
+import { useDebounce } from "./use-debounce";
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+const fetchWithZod = createZodFetcher();
+
+type Sentiment = 1 | 0 | -1;
+
+function Emoji({ sentiment }: { sentiment: Sentiment }) {
+  if (sentiment === 1) {
+    return <img src={positive} className="emoji" />;
+  }
+  if (sentiment === 0) {
+    return <img src={neutral} className="emoji" />;
+  }
+  if (sentiment === -1) {
+    return <img src={negative} className="emoji" />;
+  }
+  return <br />;
 }
 
-export default App
+function App() {
+  const [comment, setComment] = useState("");
+  const [sentiment, setSentiment] = useState<Sentiment>(0);
+  const debouncedComment = useDebounce(comment, 500);
+
+  useEffect(() => {
+    async function fetchSentiment(comment: string) {
+      const result = await fetchWithZod(
+        // The schema you want to validate with
+        zod.object({
+          sentiment: zod.union([
+            zod.literal(1),
+            zod.literal(0),
+            zod.literal(-1),
+          ]),
+        }),
+        // Any parameters you would usually pass to fetch
+        "http://192.168.1.17:4000/api/sentiment",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ data: comment }),
+        }
+      ).then((res) => {
+        return res.sentiment;
+      });
+      setSentiment(result);
+    }
+    if (debouncedComment) {
+      fetchSentiment(debouncedComment);
+    }
+  }, [debouncedComment]);
+  return (
+    <div className="App">
+      <form action="/api/sentiment">
+        <textarea
+          placeholder="What do you think"
+          value={comment}
+          onChange={(evt) => setComment(evt.target.value)}
+        ></textarea>
+        <p>{sentiment.toString()}</p>
+        <button type="submit">Submit</button>
+      </form>
+      <Emoji sentiment={sentiment} />
+    </div>
+  );
+}
+
+export default App;
